da0c014b8d1b84810965546e98de502a34e52930
axeld5 <axel.darmouni@student-cs.fr>
2024-04-14 21:06:51 +0200

    added n_iterations option

--- src/streamlit.py
+++ src/streamlit.py
@@ -1,13 +1,19 @@
-import streamlit as st
-import pandas as pd
+"""Code for the streamlit can be found here"""
 
-import numpy as np
+import streamlit as st
 from utils.wikipedia_functions import get_random_page
 from wikirace_functions import full_wikirace
 
-def generate_pages():
-    start_page = get_random_page(1)[0]
-    st.session_state.start_page = start_page
+def generate_pages(mode:str):
+    if mode == "easy":
+        start_page = get_random_page(1)[0]
+        st.session_state.start_page = start_page
+        st.session_state.end_page = "United States"
+    elif mode == "hard":
+        start_page, end_page = get_random_page(2)
+        st.session_state.start_page = start_page
+        st.session_state.end_page = end_page
+
 
 st.title("Search Agent")
 
@@ -15,17 +21,24 @@ if "start_page" not in st.session_state:
     st.session_state.start_page = ""
 
 if "end_page" not in st.session_state:
-    st.session_state.end_page = "United States"
+    st.session_state.end_page = ""
 
-generate_button = st.button("Generate Pages")
+if "n_iterations" not in st.session_state:
+    st.session_state.n_iterations = "10"
 
-if generate_button:
-    generate_pages()
+easy_mode_button = st.button("(Easy Mode) Random Start, End = United States")
+hard_mode_button = st.button("(Hard Mode) Random Start, Random End")
+
+if easy_mode_button:
+    generate_pages("easy")
+if hard_mode_button:
+    generate_pages("hard")
 
 start_page_text = st.text_input("Start Page:", value=st.session_state.start_page)
 end_page_text = st.text_input("End Page:", value=st.session_state.end_page)
+n_iterations = int(st.text_input("Number of iterations:", value=st.session_state.n_iterations))
 
 solve_button = st.button("Launch Claude-3 Solving")
 
 if solve_button:
-    full_wikirace(start_page_text, end_page_text, on_streamlit=True)
+    full_wikirace(start_page_text, end_page_text, n_iterations, on_streamlit=True)
